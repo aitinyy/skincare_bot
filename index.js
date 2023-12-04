a43cb2f8b9ce68c442e5f7134829f1b31e2614ac
aitinyy <aitanassempere@gmail.com>
2023-12-04 15:14:51 +0100

    little changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 
-app.use(express.json());
+//app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
