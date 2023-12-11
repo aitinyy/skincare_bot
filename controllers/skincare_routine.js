e0edcd61a75c07cf8408a7b5d2ea862ce40ab3dd
aitinyy <aitanassempere@gmail.com>
2023-12-11 19:49:52 +0100

    changed responses

--- controllers/skincare_routine.js
+++ controllers/skincare_routine.js
@@ -45,12 +45,12 @@ const washFace = (req,res) => {
     if(statusWash=='removeMakeup'){
         informationText = data.messagesData[1][1]["text"];
         buttonCallBack = 'washFace';
-        infoCallBack = '¿Qué jabón debería utilizar después?';
+        infoCallBack = data.messagesData[1][1]["button"];
     }
     else{
-        informationText = '¡Usar un jabón de limpieza es esencial! Deberías buscar uno que haga sentir bien a tu piel, en ningún momento debería sentirse tirante. Si tienes la piel un poco más grasa te vas a benificiar de los jabones en gel, de lo contrario uno que sea espumoso va a hacer su trabajo.';
+        informationText = data.messagesData[1][2]["text"];
         buttonCallBack = 'skinConcern';
-        infoCallBack = '¿Y si quiero tratar algo en concreto?';
+        infoCallBack = data.messagesData[1][2]["button"];
     } 
 
     return res.json({
@@ -82,8 +82,8 @@ const washFace = (req,res) => {
 const selectConcerns = (req, res) => {
 
     var concern = req.body.queryResult.parameters.skinConcern;
-    var moreConcern = ' ¿Tienes alguna preocupación más?';
-    var moreThanOne = ' Ten cuidado al tratar varios productos, primero pruebalos por separado.';
+    var moreConcern = data.messagesData[1][11]["moreConcern"];
+    var moreThanOne = data.messagesData[1][12]["moreThanOne"];
     var alreadyDone = false;
 
     speech = adjustConcern(concern);
@@ -139,28 +139,28 @@ const adjustConcern = (concern) => {
 
     switch (concern){
     case 'Acné':
-        speech = 'Podrías añadir a tu rutina algún exfoliante que contenga ácido salicílico (BHA) y/o serum que contenga niacinamida.';
+        speech = data.messagesData[1][3]["text"];
         break;
     case 'Manchas en la piel':
-        speech = 'Considera utilizar algún exfoliante AHA, cómo ácido glicólico (el más irritante pero más efectivo) o ácido láctico (punto intermedio) o ácido mandélico (el más suave pero menos efectivo).';
+        speech = data.messagesData[1][4]["text"];
         break;
     case 'Arrugas':
-        speech = 'Si quieres tratarlo de forma ligera y suave, el ácido hialurónico es ideal. Lo más efectivo son productos que contengan retinol, pero OJO puede ser un poco irritante, hay que usarlo muy poco a poco.';
+        speech = data.messagesData[1][5]["text"];
         break;
     case 'Poros':
-        speech = 'En este caso un exfoliante que contenga ácido salicílico es lo idóneo.';
+        speech = data.messagesData[1][6]["text"];
         break;
     case 'Piel sensible':
-        speech = 'En este caso se trata de evitar productos. No utilices ingredientes irritantes como ácido glicólico, ácido salicílico o ácido láctico. Evita los exofoliantes físicos, que son aquellos que contienen pequeñas particulas que se restregan por la piel. También deberías evitar los productos que contengan fragancia y aquellos que contengan alcohol (Aunque no todos los alcoholes son irritantes).';
+        speech = data.messagesData[1][7]["text"];
         break;
     case 'Piel muy seca':
-        speech = 'Convienen productos que contengan ceramidas, aceite de jojoba o manteca de karité. Ten en cuenta que este último ingrediente puede ser muy denso y puede obstruir poros, sin embargo, es muy hidratante.';
+        speech = data.messagesData[1][8]["text"];
         break;
     case 'Piel grasa':
-        speech = 'Exfoliantes que contengan ácido salicílico ayudan mucho con esta condición ya que disuelven el exceso de sebo. Por otro lado, la niacinamida ayuda a regular la producción de sebo, por lo que también es muy conveniente.';
+        speech = data.messagesData[1][9]["text"];
         break;
     case 'Textura en la piel':
-        speech = 'Cuando ocurre este tipo de condición, se recomienda usar ácido glicólico, pero este puede ser un poco irritante. Cómo alternativa está el ácido láctico. Un ingrediente que ayuda con la regeneración de la piel es el retinol, pero si vas a usarlo debes tener precaución.';
+        speech = data.messagesData[1][10]["text"];
         break;
     default:
         speech = '¿Disculpa?';
--- database/data.js
+++ database/data.js
@@ -1,8 +1,43 @@
 const messagesData = {
     1:{
         1:{
-            text : ['Es importante quitar el maquillaje correctamente. Se recomiendan los aceites desmaquillantes ya que son muy eficaces a la hora de disolver la suciedad y el maquillaje. Una alternativa menos grasienta es el agua micelar.']
-        }
+            text: ['Es importante quitar el maquillaje correctamente. Se recomiendan los aceites desmaquillantes ya que son muy eficaces a la hora de disolver la suciedad y el maquillaje. Una alternativa menos grasienta es el agua micelar.'],
+            button: ['¿Qué jabón debería utilizar después?']
+        },
+        2:{
+            text: ['¡Usar un jabón de limpieza es esencial! Deberías buscar uno que haga sentir bien a tu piel, en ningún momento debería sentirse tirante. Si tienes la piel un poco más grasa te vas a beneficiar de los jabones en gel, de lo contrario uno que sea espumoso va a hacer su trabajo.'],
+            button: ['¿Y si quiero tratar algo en concreto?']
+        },
+        3:{
+            text: ['Podrías añadir a tu rutina algún exfoliante que contenga ácido salicílico (BHA) y/o sérum que contenga niacinamida.']
+        },
+        4:{
+            text: ['Considera utilizar algún exfoliante AHA, cómo ácido glicólico (el más irritante pero más efectivo) o ácido láctico (punto intermedio) o ácido mandélico (el más suave pero menos efectivo).']
+        },
+        5:{
+            text: ['Si quieres tratarlo de forma ligera y suave, el ácido hialurónico es ideal. Lo más efectivo son productos que contengan retinol, pero OJO puede ser un poco irritante, hay que usarlo muy poco a poco.']
+        },
+        6:{
+            text: ['En este caso un exfoliante que contenga ácido salicílico es lo idóneo.']
+        },
+        7:{
+            text: ['En este caso se trata de evitar productos. No utilices ingredientes irritantes como ácido glicólico, ácido salicílico o ácido láctico. Evita los exfoliantes físicos, que son aquellos que contienen pequeñas partículas que se restriegan por la piel. También deberías evitar los productos que contengan fragancia y aquellos que contengan alcohol (Aunque no todos los alcoholes son irritantes).']
+        },
+        8:{
+            text: ['Convienen productos que contengan ceramidas, aceite de jojoba o manteca de karité. Ten en cuenta que este último ingrediente puede ser muy denso y puede obstruir poros, sin embargo, es muy hidratante.']
+        },
+        9:{
+            text: ['Exfoliantes que contengan ácido salicílico ayudan mucho con esta condición ya que disuelven el exceso de sebo. Por otro lado, la niacinamida ayuda a regular la producción de sebo, por lo que también es muy conveniente.';]
+        },
+        10:{
+            text: ['Cuando ocurre este tipo de condición, se recomienda usar ácido glicólico, pero este puede ser un poco irritante. Cómo alternativa está el ácido láctico. Un ingrediente que ayuda con la regeneración de la piel es el retinol, pero si vas a usarlo debes tener precaución.']
+        },
+        11:{
+            moreConcern: [' ¿Tienes alguna preocupación más?']
+        },
+        12:{
+            moreThanOne: [' Ten cuidado al tratar varios productos, primero pruébalos por separado.']
+        },
     }
 }
 
