69bcf942ceb7f5722783aa901bb5cf804496bbb0
aitinyy <aitanassempere@gmail.com>
2023-12-11 15:21:19 +0100

    testing

--- index.js
+++ index.js
@@ -9,26 +9,26 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-//const webhookRoute = require('./routes/webhook_routes');
+const webhookRoute = require('./routes/webhook_routes');
 //var router = require('./routes/webhook_routes');
-//app.use('/echo', webhookRoute);
+app.use('/echo', webhookRoute);
 //app.use(webhookRoute);
 
-app.post("/echo", function(req, res){
-    var speech = 'Caca';
+/*app.post("/echo", function(req, res){
+var speech = 'Caca';
 
-    return res.json({
-        "fulfillmentText": 'um',
-        "fulfillmentMessages": [
-            {
-                "text": {
-                    "text": [speech]
-                }
+return res.json({
+    "fulfillmentText": 'um',
+    "fulfillmentMessages": [
+        {
+            "text": {
+                "text": [speech]
             }
-        ],
-        "source": "<webhookpn1>"
-    });
+        }
+    ],
+    "source": "<webhookpn1>"
 });
+});*/
 
 app.listen(process.env.PORT || 8000, function(){
     console.log("Escuchando el puerto");
