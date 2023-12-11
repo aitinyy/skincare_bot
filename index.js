2005bf1a404cd9a64fb3836ea0b1ab73b1122634
aitinyy <aitanassempere@gmail.com>
2023-12-11 14:27:26 +0100

    changing this

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 "use strict";
 
 const express = require('express');
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 const app = express();
+//const router  = express.Router();
 
 //app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 const webhookRoute = require('./routes/webhook_routes');
 
