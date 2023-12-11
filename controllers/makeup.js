1f94a8018bd73df29c2da152262b833d7c219f44
aitinyy <aitanassempere@gmail.com>
2023-12-11 20:49:07 +0100

    added makeup text correctly

--- controllers/makeup.js
+++ controllers/makeup.js
@@ -1,29 +1,31 @@
+const data = require('../database/data');
+
 const manageCoveringSkin = (req, res, makeupType) => {
     var speech = '';
     switch (makeupType){
         case 'Base':
-            speech = 'La base se utiliza para tener un tono de piel unificado. Primero habría que preparar la piel con una hidratante y después un primer primer. Continuamos poniendo el producto en la parte posterior de la mano o en alguna otra superficie limpia. Con una esponja de maquillaje ligeramente húmeda, la llenamos de producto a toques. A continuación, hacemos lo mismo en la cara, a toques se va aplicando el maquillaje. Es importante desplazarse de forma uniforme por el rostro para que quede un tono unificado. Repetir hasta que esté cubierta toda la cara.';
+            speech = data.messagesData[3][1]["text"];
             break;
         case 'Primer':
-            speech = 'El primer se utiliza para preparar la piel. Se pone después de la crema hidratante. Ayudar a que el maquillaje dure y deja un textura lisa. Para empezar, se necesita un primer que trate algo que le convenga a tu tipo de piel. Se aplica como si fuera una crema.';
+            speech = data.messagesData[3][2]["text"];
             break;
         case 'Corrector':
-            speech = 'El corrector debe ser un tono más claro que tu piel. Se puede aplicar antes o después de la base, depende de gusto personal, no altera al resultado. Se utiliza para tapar manchas e imperfecciones que haya por la piel. También se utiliza para ocultar las ojeras.';
+            speech = data.messagesData[3][3]["text"];
             break;
         case 'Correctores de color':
-            speech = 'Los correctores de color suelen ayudar a unificar el tono de la piel mediante la teoría del color. El corrector naranja se ocupa de cancelar las zonas más azules/moradas cómo las ojeras, hiperpigmentación o bello facial. Según tu subtono el naranja puede ser más clarito (para subtonos verdes) o más oscuro (para subtonos azules). El corrector verde neutralizada las rojeces. El corrector azul cancela los tonos amarillos. Se pueden mezclar con tu corrector habitual para que luzca más natural.';
+            speech = data.messagesData[3][4]["text"];
             break;
         case 'Cubrir bello':
-            speech = 'En primer lugar, hay que afeitar bien la zona y asegurarse que esté bien hidratada y calmada. Luego se aplica un *primer*. Continuamos con un *corrector de color*, y en función de tu subtono se utilizará uno u otro. Subtono *verde* -> color *melocotón*. Subtono *azul* -> color *naranja*. Con una brocha pequeña aplicamos el corrector de color, únicamente donde se vea sombre. A continuación, se aplica *corrector (del tono de tu piel)* por encima. Finalmente se sella con *polvos matificantes*.';
+            speech = data.messagesData[3][5]["text"];
             break;
         case 'BB cream':
-            speech = 'Es un tipo de cobertor de la piel que contiene propiedades buenas para la piel. Tiene un poco de color y permite unificar el tono de forma ligera. Suele ser hidratante. Se aplica como si fuera una base. Se puede combinar con otros productos como el corrector o cualquier otro producto que se ocupe de realzar los rasgos faciales.';
+            speech = data.messagesData[3][6]["text"];
             break;
         case 'CC cream':
-            speech = 'Esta crema un poco de color y cubre bien, más que las BB creams. Suelen contener ingredientes de interés. Se recomiendo utilizar después de aplicar crema hidratante aunque puede sustituir a esta, preferencia personal. Se puede usar como una base y combinarla con otros productos.';
+            speech = data.messagesData[3][7]["text"];
             break;
         case 'Sellar':
-            speech = 'Para sellar el maquillaje se pueden utilizar los polvos matificantes. Se aplican tras completar el maquillaje con un aplicador por todo el rostro. Se deja actuar un momento y se retira con una brocha grande. Otro método es con spray fijador, que tan sólo hay que aplicarlo al acabar el maquillaje.';
+            speech = data.messagesData[3][8]["text"];
             break;
         default:
             speech = '¿Disculpa?';
@@ -50,13 +52,13 @@ const manageEnhancingBeauty = (req, res, makeupType2) => {
 
     switch (makeupType2){
         case 'Colorete':
-            speech = 'Hay distintas formas de utilizar el colorete. Para empezar, el tono es gusto personal, pero la posición en la cara va a influir. Para un efecto lifting, se debe aplicar por arriba de los pómulos. Para hacer más pequeña la cara, se debe aplicar en medio de la mejilla.';
+            speech = data.messagesData[3][9]["text"];
             break;
         case 'Bronceador':
-            speech = 'El bronceador se utiliza para esculpir el rostro. Debe de ser unos tonos más oscuros a tu piel y se necesita una brocha fina. Se puede usar bajo en los pómulos para marcarlos. En el puente de la nariz, dejando un hueco en medio, para una apariencia más pequeña. También en la parte superior la frente para hacerla más pequeña también. Y en el borde de la mandíbula para hacer la cara más pequeña. ';
+            speech = data.messagesData[3][10]["text"];
             break;
         case 'Iluminador':
-            speech = 'El iluminador se utiliza para realzar alguna parte de la cara. Se necesita una brocha pequeña con la que difuminar. Se puede utilizar en el centro de la nariz para hacerla más puntiaguda. También se puede poner en el puente de la nariz, haciendo una sutil línea y da la apariencia la nariz más pequeña. Se puede aplicar un poco en los labios para hacerlos más grandes. En la parte superior del pómulo da un look radiante.';
+            speech = data.messagesData[3][11]["text"];
             break;
         default:
             speech = '¿Disculpa?';
--- database/data.js
+++ database/data.js
@@ -121,6 +121,41 @@ const messagesData = {
         18:{
             text: ['Algunos de los tipos del alcohol pueden ser irritantantes para la piel como el alcohol denat, sobretodo para la gente con piel sensible.']
         }
+    },
+    3:{
+        1:{
+            text: ['La base se utiliza para tener un tono de piel unificado. Primero habría que preparar la piel con una hidratante y después un primer primer. Continuamos poniendo el producto en la parte posterior de la mano o en alguna otra superficie limpia. Con una esponja de maquillaje ligeramente húmeda, la llenamos de producto a toques. A continuación, hacemos lo mismo en la cara, a toques se va aplicando el maquillaje. Es importante desplazarse de forma uniforme por el rostro para que quede un tono unificado. Repetir hasta que esté cubierta toda la cara.']
+        },
+        2:{
+            text: ['El primer se utiliza para preparar la piel. Se pone después de la crema hidratante. Ayudar a que el maquillaje dure y deja un textura lisa. Para empezar, se necesita un primer que trate algo que le convenga a tu tipo de piel. Se aplica como si fuera una crema.']
+        },
+        3:{
+            text: ['El corrector debe ser un tono más claro que tu piel. Se puede aplicar antes o después de la base, depende de gusto personal, no altera al resultado. Se utiliza para tapar manchas e imperfecciones que haya por la piel. También se utiliza para ocultar las ojeras.']
+        },
+        4:{
+            text: ['Los correctores de color suelen ayudar a unificar el tono de la piel mediante la teoría del color. El corrector naranja se ocupa de cancelar las zonas más azules/moradas cómo las ojeras, hiperpigmentación o bello facial. Según tu subtono el naranja puede ser más clarito (para subtonos verdes) o más oscuro (para subtonos azules). El corrector verde neutralizada las rojeces. El corrector azul cancela los tonos amarillos. Se pueden mezclar con tu corrector habitual para que luzca más natural.']
+        },
+        5:{
+            text: ['En primer lugar, hay que afeitar bien la zona y asegurarse que esté bien hidratada y calmada. Luego se aplica un *primer*. Continuamos con un *corrector de color*, y en función de tu subtono se utilizará uno u otro. Subtono *verde* -> color *melocotón*. Subtono *azul* -> color *naranja*. Con una brocha pequeña aplicamos el corrector de color, únicamente donde se vea sombre. A continuación, se aplica *corrector (del tono de tu piel)* por encima. Finalmente se sella con *polvos matificantes*.']
+        },
+        6:{
+            text: ['Es un tipo de cobertor de la piel que contiene propiedades buenas para la piel. Tiene un poco de color y permite unificar el tono de forma ligera. Suele ser hidratante. Se aplica como si fuera una base. Se puede combinar con otros productos como el corrector o cualquier otro producto que se ocupe de realzar los rasgos faciales.']
+        },
+        7:{
+            text: ['Esta crema un poco de color y cubre bien, más que las BB creams. Suelen contener ingredientes de interés. Se recomiendo utilizar después de aplicar crema hidratante aunque puede sustituir a esta, preferencia personal. Se puede usar como una base y combinarla con otros productos.']
+        },
+        8:{
+            text: ['Para sellar el maquillaje se pueden utilizar los polvos matificantes. Se aplican tras completar el maquillaje con un aplicador por todo el rostro. Se deja actuar un momento y se retira con una brocha grande. Otro método es con spray fijador, que tan sólo hay que aplicarlo al acabar el maquillaje.']
+        },
+        9:{
+            text: ['Hay distintas formas de utilizar el colorete. Para empezar, el tono es gusto personal, pero la posición en la cara va a influir. Para un efecto lifting, se debe aplicar por arriba de los pómulos. Para hacer más pequeña la cara, se debe aplicar en medio de la mejilla.']
+        },
+        10:{
+            text: ['El bronceador se utiliza para esculpir el rostro. Debe de ser unos tonos más oscuros a tu piel y se necesita una brocha fina. Se puede usar bajo en los pómulos para marcarlos. En el puente de la nariz, dejando un hueco en medio, para una apariencia más pequeña. También en la parte superior la frente para hacerla más pequeña también. Y en el borde de la mandíbula para hacer la cara más pequeña. ']
+        },
+        11:{
+            text: ['El iluminador se utiliza para realzar alguna parte de la cara. Se necesita una brocha pequeña con la que difuminar. Se puede utilizar en el centro de la nariz para hacerla más puntiaguda. También se puede poner en el puente de la nariz, haciendo una sutil línea y da la apariencia la nariz más pequeña. Se puede aplicar un poco en los labios para hacerlos más grandes. En la parte superior del pómulo da un look radiante.']
+        }
     }
 }
 
